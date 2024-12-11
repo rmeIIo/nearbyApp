d57b8615f7458d9affbe9a89675e74ab278bcad0
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2024-12-11 10:21:48 -0300

    feat: added welcome component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
 import { View, Text } from "react-native";
 
+import { Welcome } from "@/components/welcome";
+
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 40,
+        gap: 40,
       }}
     >
-      <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+      <Welcome />
     </View>
   );
 }
