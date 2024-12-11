4515dbf1ece64443950b0afc3f7aa0447e436ed2
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2024-12-11 20:57:41 -0300

    feat: added button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
@@ -14,6 +15,9 @@ export default function Index() {
     >
       <Welcome />
       <Steps />
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
